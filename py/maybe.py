8b3980da7865761301749a0d25137ffcc4483841
hazza <harrystenholm@yahoo.com>
2023-11-15 10:02:47 +0000

    python playabout

--- py/maybe.py
+++ py/maybe.py
@@ -3,10 +3,15 @@ from bs4 import BeautifulSoup as bs
 
 url = ("https://www.goodreads.com/review/list_rss/171499739?key=Q2SS70nHhSbpMIZdEDFXpoVzWuLSbnDn5-dyYwjGS1OO6Yzp&shelf=%23ALL%23")
 harry = ("https://harrystenholm.github.io/main/")
-page = requests.get(harry)
 
-blah = bs(page.content, "html.parser")
+pageharry = requests.get(harry)
+blahharry = bs(pageharry.content, "html.parser")
 
+#print(blahharry.h1)
 
-print(blah.h1)
+page = requests.get(url)
+tag = (page.title)
+
+print(tag)
+
 
